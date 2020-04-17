cf4544cbbe2fa86d3045310c2da1d739a5678ee9
Cesar Toribio <ctoribio@versalushealth.com>
2020-04-17 12:10:19 -0400

    Added a custom middleware to facilitate the book load for get by id, put and patch.

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
 const express = require('express');
 
 
@@ -19,13 +21,64 @@ function routes(Book) {
       });
     });
 
+  // middleware for load a book.
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  });
+
   bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-      Book.findById(req.params.bookId, (err, books) => {
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
+      book.isbn = req.body.isbn;
+      book.title = req.body.title;
+      book.subtitle = req.body.subtitle;
+      book.author = req.body.author;
+      book.published = req.body.published;
+      book.publisher = req.body.publisher;
+      book.pages = req.body.pages;
+      book.description = req.body.description;
+      book.website = req.body.website;
+      book.read = req.body.read || false;
+      req.book.save((err) => {
         if (err) {
           return res.send(err);
         }
-        return res.json(books);
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const k = item[0];
+        const v = item[1];
+        book[k] = v;
+      });
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
       });
     });
   return bookRouter;
