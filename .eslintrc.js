bcbc7cde7c19223efea7569f0bace7cfcb9eb0ed
Cesar Toribio <17240232+cesarjuniort@users.noreply.github.com>
2020-04-16 23:18:25 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules":{
+        // disable rules from base configurations
+        "no-console": "off",
+        "linebreak-style": "off",
+    }
+};
