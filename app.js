af474718219fb0a1f110d123ad2914f39edf691d
Cesar Toribio <ctoribio@versalushealth.com>
2020-04-17 13:57:10 -0400

    Added a sample of Integration Test.

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
-// eslint-disable-next-line no-unused-vars
-const db = mongoose.connect('mongodb://localhost/bookAPI');
+if (process.env.ENV === 'Test') {
+  console.log('Running on a TEST environment');
+  // eslint-disable-next-line no-unused-vars
+  const db = mongoose.connect('mongodb://localhost/bookAPI_Test');
+} else {
+  console.log('Running on a Production environment');
+  // eslint-disable-next-line no-unused-vars
+  const db = mongoose.connect('mongodb://localhost/bookAPI');
+}
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
 
@@ -20,6 +27,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to my API!');
 });
 
-app.listen(port, () => {
+app.server = app.listen(port, () => {
   console.log(`Node JS Express Server started on Port: ${port}`);
 });
+
+module.exports = app;
