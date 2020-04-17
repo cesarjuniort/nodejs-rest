70bc490efb5a2278f954b895f4f33d477284c1f9
Cesar Toribio <ctoribio@versalushealth.com>
2020-04-17 11:28:54 -0400

    Refactored the book routes.

--- app.js
+++ app.js
@@ -3,42 +3,18 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+// eslint-disable-next-line no-unused-vars
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
-const appRouter = express.Router();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-appRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const { query } = req;
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
 
-appRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-app.use('/api', appRouter);
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my API!');
---
+++ routes/bookRouter.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+
+function routes(Book) {
+  const bookRouter = express.Router();
+  bookRouter.route('/books')
+    .post((req, res) => {
+      const book = new Book(req.body);
+      book.save();
+      return res.status(201).json(book);
+    })
+    .get((req, res) => {
+      const { query } = req;
+      Book.find(query, (err, books) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(books);
+      });
+    });
+
+  bookRouter.route('/books/:bookId')
+    .get((req, res) => {
+      Book.findById(req.params.bookId, (err, books) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(books);
+      });
+    });
+  return bookRouter;
+}
+
+module.exports = routes;
